aea0b4dd93f1e962926ab23bcb490951a466606b
AnishKhotTaurus <82097179+anishkhotgithub@users.noreply.github.com>
2026-02-23 13:21:18 +0530

    ci/cd added

--- src/api/services/appointment.ts
+++ src/api/services/appointment.ts
@@ -22,7 +22,6 @@ export default class RoleService {
   async createAppointment(req: any): Promise<{ appointment: any }> {
     let doc = {};
     const zod = CreateAppointmentSchema.parse(req);
-    console.log("zod", zod);
     doc = await this.appointmentModel.create(zod);
     return { appointment: doc };
   }
